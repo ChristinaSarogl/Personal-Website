4024aa641313fb6789e55f1c894de7cefd4e7eba
ChristinaSarogl <xristsar@gmail.com>
2023-06-22 07:37:45 +0300

    Tmeline UI fix

--- js/navBar.js
+++ js/navBar.js
@@ -11,3 +11,29 @@ function closeMenu(){
    let menu = document.getElementsByClassName("navbar-collapse");
    menu[0].classList.toggle("show");
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+   document.querySelectorAll('.modal-btn').forEach(item => {
+      item.addEventListener('click', event => {
+
+         let elemName = event.target.className;
+         let elem = event.target;
+         if (elemName.includes('bi')){
+            elem = event.target.parentElement;
+         } else if(elemName.includes('label')){
+            elem = event.target.parentElement.parentElement;
+         }
+
+         elem.setAttribute(
+            'aria-expanded', 
+            elem.getAttribute('aria-expanded') === 'true'? 'false': 'true'
+         );
+         elem.classList.toggle('open');
+
+         // Open modal
+         let modal = document.querySelector(elem.getAttribute('data-target'));
+         modal.classList.toggle('show');
+         modal.classList.toggle('hide');
+      });
+   });
+});
