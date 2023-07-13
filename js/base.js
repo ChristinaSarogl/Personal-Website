86d26bcf2ea2e6e68f500afcab7522312bee020b
Christina Saroglaki <xristsar@gmail.com>
2023-07-13 13:16:36 +0300

    Added a set height to timeline

--- js/base.js
+++ js/base.js
@@ -12,26 +12,35 @@ function closeMenu() {
   menu[0].classList.toggle("show");
 }
 
+function changeModalVisibility(item) {
+  item.setAttribute(
+    "aria-expanded",
+    item.getAttribute("aria-expanded") === "true" ? "false" : "true"
+  );
+  item.toggleAttribute("disabled");
+
+  let details = document.getElementById(item.getAttribute("data-bs-target"));
+  details.classList.toggle("hide");
+  Array.from(details.getElementsByClassName("fade-in")).forEach((text) => {
+    text.classList.toggle("show");
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  let timelineCard = document.getElementsByClassName("side-menu_container")[0];
+  timelineCard.style.minHeight = timelineCard.offsetHeight + "px";
+
   document.querySelectorAll(".side-menu_btn").forEach((item) => {
     item.addEventListener("click", (event) => {
       let elem = event.target;
 
-      Array.from(elem.parentNode.children).forEach(item => {
-        if (item.getAttribute("aria-expanded")) {
-          let details = document.getElementById(item.getAttribute("data-bs-target"));
-          details.classList.toggle('hide');
-          Array.from(details.getElementsByClassName("fade-in")).forEach(text => {
-            text.classList.toggle('show');
-          });
+      Array.from(elem.parentNode.children).forEach((item) => {
+        if (item.getAttribute("aria-expanded") === "true") {
+          changeModalVisibility(item);
         }
-
-        item.setAttribute(
-          "aria-expanded",
-          item.getAttribute("aria-expanded") === "true" ? "false" : "true"
-        );
-        item.toggleAttribute("disabled");
       });
+
+      changeModalVisibility(elem);
     });
   });
 });
