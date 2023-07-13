c2c9ac2dd1f003a4c2096020946be0fefae6db8e
Christina Saroglaki <xristsar@gmail.com>
2023-07-13 11:31:50 +0300

    Timeline styling

--- js/base.js
+++ js/base.js
@@ -1,57 +1,55 @@
-function menuButtonAnimation(){
-   let button = document.getElementsByClassName("mobile-menu-button");
-   button[0].classList.toggle("active");
+function menuButtonAnimation() {
+  let button = document.getElementsByClassName("mobile-menu-button");
+  button[0].classList.toggle("active");
 }
 
-function closeMenu(){
-   let button = document.getElementsByClassName("mobile-menu-button");
-   button[0].classList.toggle("collapsed");
-   button[0].classList.toggle("active");
+function closeMenu() {
+  let button = document.getElementsByClassName("mobile-menu-button");
+  button[0].classList.toggle("collapsed");
+  button[0].classList.toggle("active");
 
-   let menu = document.getElementsByClassName("navbar-collapse");
-   menu[0].classList.toggle("show");
+  let menu = document.getElementsByClassName("navbar-collapse");
+  menu[0].classList.toggle("show");
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-   document.querySelectorAll('.modal-btn').forEach(item => {
-      item.addEventListener('click', event => {
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".side-menu_btn").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let elem = event.target;
 
-         let elemName = event.target.className;
-         let elem = event.target;
-         if (elemName.includes('bi')){
-            elem = event.target.parentElement;
-         } else if(elemName.includes('label')){
-            elem = event.target.parentElement.parentElement;
-         }
+      Array.from(elem.parentNode.children).forEach(item => {
+        if (item.getAttribute("aria-expanded")) {
+          let details = document.getElementById(item.getAttribute("data-bs-target"));
+          details.classList.toggle('hide');
+          Array.from(details.getElementsByClassName("fade-in")).forEach(text => {
+            text.classList.toggle('show');
+          });
+        }
 
-         elem.setAttribute(
-            'aria-expanded', 
-            elem.getAttribute('aria-expanded') === 'true'? 'false': 'true'
-         );
-         elem.classList.toggle('open');
-
-         // Open modal
-         let modal = document.querySelector(elem.getAttribute('data-target'));
-         modal.classList.toggle('show');
-         modal.classList.toggle('hide');
+        item.setAttribute(
+          "aria-expanded",
+          item.getAttribute("aria-expanded") === "true" ? "false" : "true"
+        );
+        item.toggleAttribute("disabled");
       });
-   });
-
-   document.querySelectorAll('.carouselCard').forEach(item => {
-      item.addEventListener('click', event =>{
-         let elemName = event.target.className;
-         let elem = event.target;
-
-         if (!elemName.includes('carouselCard')){
-            elem = event.target.parentElement;
-         }
-
-         console.log(elem);
-         console.log(elem.querySelector('button'));
-         let activeButton = elem.getElementsByTagName('button')[0];
-         if (activeButton.disabled){
-            activeButton.disabled = false;
-         }
-      })
-   });
+    });
+  });
 });
+
+// document.querySelectorAll('.carouselCard').forEach(item => {
+//    item.addEventListener('click', event =>{
+//       let elemName = event.target.className;
+//       let elem = event.target;
+
+//       if (!elemName.includes('carouselCard')){
+//          elem = event.target.parentElement;
+//       }
+
+//       console.log(elem);
+//       console.log(elem.querySelector('button'));
+//       let activeButton = elem.getElementsByTagName('button')[0];
+//       if (activeButton.disabled){
+//          activeButton.disabled = false;
+//       }
+//    })
+// });
